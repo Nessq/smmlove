57454763fe09452144346849bd7cf2a658e4d2b8
Nessq <Nessq95@gmail.com>
2024-12-05 15:14:11 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import { i18n } from './next-i18next.config';
-
-const nextConfig = {
-    i18n,
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/api/users/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '@/lib/db'
-import { users } from '@/entities/user/schema'
-
-export async function GET(request: Request) {
-	const database = await db
-	const data = await database.select().from(users).execute()
-
-	return Response.json(data)
-}
